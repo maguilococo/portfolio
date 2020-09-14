903ba65564c478c2fb5a9a92faaec993a657c800
maguilococo <56405247+maguilococo@users.noreply.github.com>
2020-09-14 16:00:47 -0300

    First commit from local repository

---
+++ app.js
@@ -0,0 +1,24 @@
+//HomePage
+
+
+//Proyectos
+let gitBtn = document.getElementById("git-btn");
+
+//git_btn.onclick = () => {
+
+//Contact
+let mailContact = document.getElementById("mail-contact");
+let linkedinContact = document.getElementById("linkedin-contact");
+let gitContact = document.getElementById("git-contact");
+
+mailContact.onclick = () => {
+    mailContact.src = "images/mail_footer.svg";
+}
+
+linkedinContact.onclick = () => {
+    linkedinContact.src = "images/linkedin-click.svg";
+}
+
+gitContact.onclick = () => {
+    gitContact.src = "images/github-click.svg";
+}
