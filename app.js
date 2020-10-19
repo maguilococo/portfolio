0b8959eb67029620d492d199c58e4c0d82d97410
maguilococo <56405247+maguilococo@users.noreply.github.com>
2020-10-19 14:37:03 -0300

    Responsive optimization
    Include Bootsrap 4 classes to improve the responsiveness

--- app.js
+++ app.js
@@ -5,25 +5,25 @@ function visitUrl (url) {
     window.open(url, '_blank');
 }
 
-
-//PORTFOLIO "https://github.com/maguilococo/portfolio" "http://htmlpreview.github.io/?https://github.com/maguilococo/portfolio/blob/master/index.html"
-//PAPER "https://github.com/maguilococo/workshopJS" "https://htmlpreview.github.io/?https://github.com/maguilococo/workshopJS/blob/master/index.html"
-//PIANO "https://github.com/maguilococo/piano"   
-//CS50 "https://github.com/me50/maguilococo"
-
 //Contact
 let mailContact = document.getElementById("mail-contact");
 let linkedinContact = document.getElementById("linkedin-contact");
 let gitContact = document.getElementById("git-contact");
 
 mailContact.onclick = () => {
-    mailContact.src = "images/mail_footer.svg";
+    setTimeout(() => {
+        mailContact.src = "images/mail_footer.svg";
+    }, 2000);
 }
 
 linkedinContact.onclick = () => {
-    linkedinContact.src = "images/linkedin-click.svg";
+    setTimeout(() => {
+        linkedinContact.src = "images/linkedin-click.svg";
+    }, 2000);
 }
 
 gitContact.onclick = () => {
-    gitContact.src = "images/github-click.svg";
+    setTimeout(() => {
+        gitContact.src = "images/github-click.svg";
+    }, 2000);
 }
